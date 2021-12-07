6540ffd226f075cb9f3a1c0d7b107280405ca40e
Solo_jf68 <Solo_jf68@72-10>
2021-12-07 16:10:14 +0200

    Added mousemove

--- Hooks/Hooks.cpp
+++ Hooks/Hooks.cpp
@@ -3,6 +3,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "framework.h"
 #include "Hooks.h"
+#include <Windowsx.h>
+#include <Windows.h>
+#include <CommCtrl.h>
 
 
 #define MAX_LOADSTRING 100
@@ -10,6 +13,15 @@
 #define CMD_KB_HOOK_STOP 1002
 #define CMD_KB_HOOK_LOW_START 1003
 #define CMD_KB_HOOK_LOW_STOP 1004
+#define CMD_MOUSE_HOOK_LOW_START 1005
+#define CMD_MOUSE_HOOK_LOW_STOP 1006
+
+#define MS_OFFSET_X      5
+#define MS_OFFSET_Y      5
+#define MS_SCALE_X       5
+#define MS_SCALE_Y       5
+#define MS_CLICK_SIZE    6
+
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
@@ -17,10 +29,24 @@ WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 WCHAR str[MAX_LOADSTRING];
 HWND list;                                      // Console
+HWND msTrace;
 FILE* f;
 HHOOK kbhook;                                   // Hook handle for KB
 HHOOK LOWkbhook;                                   // Hook handle for KB
-                                               // Hook handle for KB
+HHOOK MouseLL;         
+BOOL  firstMove;
+
+HDC msTraceDC;
+
+int pen_size = 20;
+int red = RGB(255, 0, 0);
+int blue = RGB(0, 0, 255);
+int black = RGB(0, 0, 0);
+int white = RGB(255, 255, 255);
+int current_color;
+HPEN pen = CreatePen(PS_SOLID, 20, black);
+bool mouse_move;
+bool MLB_Press = false;
 
 int buffCount = 0;
 
@@ -37,6 +63,11 @@ LRESULT CALLBACK      KbHookProc(int, WPARAM, LPARAM);
 DWORD CALLBACK      LOWStartKbHook(LPVOID);
 DWORD CALLBACK      LOWStopKbHook(LPVOID);
 LRESULT CALLBACK    LOWKbHookProc(int, WPARAM, LPARAM);
+
+DWORD CALLBACK      StartMouseHook(LPVOID);
+DWORD CALLBACK      StopMouseHook(LPVOID);
+LRESULT CALLBACK    MouseHookProc(int, WPARAM, LPARAM);
+
 
 void SaveToFile(char*);
 
@@ -123,6 +154,8 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
+   
+
    if (!hWnd)
    {
       return FALSE;
@@ -150,6 +183,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     {
     case WM_CREATE:
     {
+       
         list = CreateWindowW(L"Listbox", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | WS_VSCROLL,
             120, 10, 200, 300, hWnd, 0, hInst, NULL);
         CreateWindowW(L"Button", L"KB Start", WS_CHILD | WS_VISIBLE,
@@ -160,6 +194,18 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             10, 70, 100, 23, hWnd, (HMENU)CMD_KB_HOOK_LOW_START, hInst, NULL);
         CreateWindowW(L"Button", L"KB LOW Stop", WS_CHILD | WS_VISIBLE,
             10, 100, 100, 23, hWnd, (HMENU)CMD_KB_HOOK_LOW_STOP, hInst, NULL);
+        CreateWindowW(L"Button", L"Mouse Hook Start", WS_CHILD | WS_VISIBLE,
+            10, 130, 100, 23, hWnd, (HMENU)CMD_MOUSE_HOOK_LOW_START, hInst, NULL);
+        CreateWindowW(L"Button", L"Mouse Hook Stop", WS_CHILD | WS_VISIBLE,
+            10, 160, 100, 23, hWnd, (HMENU)CMD_MOUSE_HOOK_LOW_STOP, hInst, NULL);
+
+        msTrace = CreateWindowExW(0, L"Static", L"", WS_CHILD | WS_VISIBLE | SS_ETCHEDFRAME ,
+            350, 10, 300, 250, hWnd, 0, hInst, NULL);
+        msTraceDC = GetDC(msTrace);
+
+        kbhook = LOWkbhook;
+        MouseLL = 0;
+        break;
     }
     case WM_COMMAND:
         {
@@ -183,7 +229,16 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             case CMD_KB_HOOK_LOW_STOP:
                 LOWStopKbHook(NULL);
                 break;
+
+            case CMD_MOUSE_HOOK_LOW_START:
+                StartMouseHook(NULL);
+                break;
+
+            case CMD_MOUSE_HOOK_LOW_STOP:
+                StopMouseHook(NULL);
+                break;
           
+           
             case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
                 break;
@@ -195,6 +250,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
         }
         break;
+    
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
@@ -204,6 +260,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
     case WM_DESTROY:
+       
         PostQuitMessage(0);
         break;
     default:
@@ -277,7 +334,6 @@ LRESULT CALLBACK KbHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
 
 }
 
-
 DWORD CALLBACK  LOWStartKbHook(LPVOID params) {
     LOWkbhook = SetWindowsHookExW(WH_KEYBOARD_LL, LOWKbHookProc, GetModuleHandle(NULL), 0);
     if (LOWkbhook != 0) {
@@ -313,6 +369,7 @@ DWORD CALLBACK  LOWStopKbHook(LPVOID params) {
     return 0;
 
 }
+
 
 void SaveToFile(char* buff) {
 
@@ -351,4 +408,98 @@ LRESULT CALLBACK LOWKbHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
     }
     return CallNextHookEx(LOWkbhook, nCode, wParam, lParam);
 
+}
+
+
+DWORD CALLBACK  StartMouseHook(LPVOID params) {
+   
+    if (MouseLL != 0) {
+        _snwprintf(str, MAX_LOADSTRING, L"Start fail");
+      
+
+    }
+    else
+    {
+        MouseLL = SetWindowsHookExW(WH_MOUSE_LL, MouseHookProc, (HINSTANCE)GetModuleHandle(NULL), 0);
+        _snwprintf(str, MAX_LOADSTRING, L"Started");
+
+    }
+    SendMessage(list, LB_ADDSTRING, 0, (LPARAM)str);
+    return 0;
+
+}
+
+DWORD CALLBACK  StopMouseHook(LPVOID params) {
+
+    UnhookWindowsHookEx(MouseLL);
+    if (MouseLL != 0) {
+
+        _snwprintf(str, MAX_LOADSTRING, L"Stoped");
+
+    }
+    else {
+
+        _snwprintf(str, MAX_LOADSTRING, L"Hook is not active");
+
+    }
+    SendMessage(list, LB_ADDSTRING, 0, (LPARAM)str);
+    
+    return 0;
+
+}
+
+LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode == HC_ACTION) {
+        MOUSEHOOKSTRUCT msInfo = *((MOUSEHOOKSTRUCT*)lParam);
+        switch (wParam) {
+        case WM_LBUTTONDOWN: {
+            _snwprintf_s(str, MAX_LOADSTRING,
+                L"%d %d", msInfo.pt.x, msInfo.pt.y);
+
+            SendMessageW(list, LB_ADDSTRING, 0, (LPARAM)str);
+
+            Ellipse(msTraceDC,
+                MS_OFFSET_X + msInfo.pt.x / MS_SCALE_X - MS_CLICK_SIZE / 2,
+                MS_OFFSET_Y + msInfo.pt.y / MS_SCALE_Y - MS_CLICK_SIZE / 2,
+
+                MS_OFFSET_X + msInfo.pt.x / MS_SCALE_X + MS_CLICK_SIZE / 2,
+                MS_OFFSET_Y + msInfo.pt.y / MS_SCALE_Y + MS_CLICK_SIZE / 2
+            );
+            break;
+        }
+        case WM_RBUTTONDOWN: {
+            _snwprintf_s(str, MAX_LOADSTRING,
+                L"%d %d", msInfo.pt.x, msInfo.pt.y);
+
+            SendMessageW(list, LB_ADDSTRING, 0, (LPARAM)str);
+
+            Rectangle(msTraceDC,
+                MS_OFFSET_X + msInfo.pt.x / MS_SCALE_X - MS_CLICK_SIZE / 2,
+                MS_OFFSET_Y + msInfo.pt.y / MS_SCALE_Y - MS_CLICK_SIZE / 2,
+
+                MS_OFFSET_X + msInfo.pt.x / MS_SCALE_X + MS_CLICK_SIZE / 2,
+                MS_OFFSET_Y + msInfo.pt.y / MS_SCALE_Y + MS_CLICK_SIZE / 2
+            );
+            break;
+        }
+        case WM_MOUSEMOVE: {
+            if (firstMove) {
+                MoveToEx(msTraceDC,
+                    MS_OFFSET_X + msInfo.pt.x / MS_SCALE_X,
+                    MS_OFFSET_Y + msInfo.pt.y / MS_SCALE_Y,
+                    NULL);
+                firstMove = FALSE;
+            }
+         
+            else {
+                LineTo(msTraceDC,
+                    MS_OFFSET_X + msInfo.pt.x / MS_SCALE_X,
+                    MS_OFFSET_Y + msInfo.pt.y / MS_SCALE_Y);
+            }
+            break;
+        }
+        }
+    }
+
+    return CallNextHookEx(MouseLL, nCode, wParam, lParam);
 }
